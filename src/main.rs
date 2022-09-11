3a6986056acf7c7a41258e955d42a57b9c2c8908
Hamid R. K. Pisghadam <hamidrkp@riseup.net>
2022-09-12 01:07:18 +0430

    add cli to fix subtitles

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use sub_fixer::Subtitle;
+
+use clap::Parser;
+use std::{fs::read_to_string, path::PathBuf};
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// The subtitle file to fix
+    file_name: PathBuf,
+}
+
+fn main() -> std::io::Result<()> {
+    let args: Args = Args::parse();
+
+    let input = read_to_string(&args.file_name)?;
+
+    let subtitle = Subtitle::new(input);
+    let output = subtitle.fix();
+    let fixed_subtitle_file_name = args.file_name.with_extension("fixed.srt");
+
+    std::fs::write(fixed_subtitle_file_name, output)
 }
